1cf0cb07798ab313e27f38df590d9c6260c12ec1
unknown <rangav0454@gmail.com>
2024-05-11 09:08:22 +0530

    hello script

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,6 +2,6 @@
 
 Movies=("rrr" "bheema" "rebel" "power")
 
-echo "first movie is: $Movies[0]"
-echo "first movie is: $Movies[1]"
-echo "first movie is: $Movies[@]"
+echo "first movie is: ${Movies[0]}"
+echo "first movie is: ${Movies[1]}"
+echo "first movie is: ${Movies[@]}"
