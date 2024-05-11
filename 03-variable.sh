e3555ccc230772fbf21e5241f18cc30fcd38c3a0
unknown <rangav0454@gmail.com>
2024-05-11 09:01:18 +0530

    hello script

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ravi
+person2=ravus
+
+echo "ranga:: hi ramesh, h r u?"
+echo "ramesh:: fyn ranga, wt about you?"
+echo "ranga:: im good, thanks for asking"
+echo "ramesh:: its my pleassure
