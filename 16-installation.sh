35ade0a360766c61ee882bc7fcea83828d3ffcd1
unknown <rangav0454@gmail.com>
2024-05-11 11:47:39 +0530

    hello script

---
+++ 16-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id-u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root acces."
+    exit 1
+else
+    echo "you are super user"
+fi
+
+dnf install Ansible -y
+echo "is script proceeding"
