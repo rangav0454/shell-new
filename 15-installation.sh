29123a837498d84f0aea910d972a8d410940d143
unknown <rangav0454@gmail.com>
2024-05-11 11:37:45 +0530

    hello script

---
+++ 15-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id-u)
+
+if [ $USERID -ne 0 ]
+then
+    echo " Please run this script with root access."
+    exit 1
+else
+    echo " You are super user."
+
+dnf install mysql -y
+echo "is script proceeding?"
